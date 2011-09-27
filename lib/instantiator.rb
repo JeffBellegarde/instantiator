2a8057b503ece4cab654c948826aab5197afbc8e
Jeff Bellegarde <bellegar@gmail.com>
2011-09-26 22:30:36 -0700

    Used a local variable for partials.

--- lib/instantiator.rb
+++ lib/instantiator.rb
@@ -6,14 +6,15 @@ module Instantiator
     self.send(:define_method, name) do
       self.instance_variable_set(:@instances, {}) unless self.instance_variable_defined? :@instances
       self.instance_variable_set(:@partials, {}) unless self.instance_variable_defined? :@partials
+      partials = self.instance_variable_get(:@partials)
       self.instance_variable_get(:@instances)[name] ||=
         begin
-          if self.instance_variable_get(:@partials)[name] 
-            self.instance_variable_get(:@partials)[name] 
+          if partials[name] 
+            partials[name] 
           else
-            self.instance_variable_get(:@partials)[name] = LazyProxy.new(&block)
+            partials[name] = LazyProxy.new(&block)
             instance = self.instance_eval(&block)
-            self.instance_variable_get(:@partials)[name] = nil
+            partials[name] = nil
             instance
           end
         end
